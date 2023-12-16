4b18af30c0340244a96f9aec54666ecf79ed5d1c
Felipe De Andrade <felipe.deam98@gmail.com>
2023-12-16 04:24:05 -0300

    Aula 2 concluída

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-import http from 'http';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-  '/': 'Curso de Node.js',
-  '/livros': 'Rota dos Livros',
-  '/autores': 'Rota de Autores não a muitos',
-}
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain',
-  })
-  res.end(rotas[req.url]);
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('Servidor rodando!');
-})
+});
 
