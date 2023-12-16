3f52c34ad17aa17e10a2baa282b4d1b0fc1fe8b1
Felipe De Andrade <felipe.deam98@gmail.com>
2023-12-16 13:10:03 -0300

    Início da Aula 04

--- src/app.js
+++ src/app.js
@@ -1,31 +1,27 @@
 import express from 'express';
+import conecaNaDatabase from './config/dbConnect.js';
+import livro from './models/Livro.js';
+
+const conexao = await conecaNaDatabase();
+
+conexao.on('error', (erro) => {
+  console.error('Erro de conexão', erro);
+});
+
+conexao.once('open', () => {
+  console.log('Conexão com o banco feita com sucesso!')
+});
 
 const app = express();
 app.use(express.json());
-
-const livros = [
-  {
-    id: 1,
-    titulo: "O Senhos dos Anéis"
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit"
-  }
-]
-
-function buscaLivro(id) {
-  return livros.findIndex(livro => {
-    return livro.id === Number(id);
-  });
-}
 
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node.js');
 });
 
-app.get('/livros', (req, res) => {
-  res.status(200).json(livros);
+app.get('/livros', async (req, res) => {
+  const listaLivros = await livro.find({})
+  res.status(200).json(listaLivros);
 });
 
 app.get('/livros/:id', (req, res) => {
---
+++ src/config/dbConnect.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import mongoose, { connect } from 'mongoose';
+
+async function conecaNaDatabase() {
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
+  return mongoose.connection;
+};
+
+export default conecaNaDatabase;
+
+
